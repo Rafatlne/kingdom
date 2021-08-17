134aa641fd8f8f37afcfea5740d426815211d357
Rafatlne <ashikrafat.lne@gmail.com>
2021-08-17 16:11:43 +0600

    added Language, Neighbour, Country model

--- countries/models.py
+++ countries/models.py
@@ -1,20 +1,8 @@
 from django.db import models
 
 
-class Country(models.Model):
-    name = models.CharField(max_length=200)
-    alpha2Code = models.CharField(max_length=10)
-    capital = models.CharField(max_length=200)
-    population = models.CharField(max_length=200, blank=True)
-    timezone = models.CharField(max_length=200, blank=True)
-    flag = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.name
-
 class Language(models.Model):
     name = models.CharField(max_length=200)
-    countries = models.ManyToManyField(Country, related_name="languages")
 
     def __str__(self):
         return self.name
@@ -22,7 +10,19 @@ class Language(models.Model):
 class Neighbour(models.Model):
     name = models.CharField(max_length=200)
     alpha3Code = models.CharField(max_length=10)
-    countries = models.ManyToManyField(Country, related_name="neighbours")
 
     def __str__(self):
         return self.name
+class Country(models.Model):
+    name = models.CharField(max_length=200)
+    alpha2Code = models.CharField(max_length=10)
+    capital = models.CharField(max_length=200)
+    population = models.CharField(max_length=200, blank=True)
+    timezone = models.CharField(max_length=200, blank=True)
+    flag = models.TextField(blank=True)
+    languages = models.ManyToManyField(Language, related_name="countries")
+    neighbours = models.ManyToManyField(Neighbour, related_name="countries")
+
+    def __str__(self):
+        return self.name
+
